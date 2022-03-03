3c11701319cc37ff13b6268ead1bc1244bc633a8
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-03-02 22:08:07 -0300

    project finished

--- src/components/PredefinedFoodDishes.js
+++ src/components/PredefinedFoodDishes.js
@@ -5,13 +5,19 @@ import { predefinedFoodDishes } from "../constants";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 
 const PredefinedFoodDishes = ({ getData, setHits }) => {
-  
   const getFoods = (title) => {
-
+    predefinedFoodDishes.map((foodDish) => {
+      foodDish.selected = foodDish.title === title ? !foodDish.selected : false;
+      return !foodDish.selected
+        ? setHits()
+        : getData(
+            document.querySelector(`#${title}`).value.toLowerCase().trim()
+          );
+    });
   };
 
   return (
-    <Grid item xs={ 12 }>
+    <Grid item xs={12}>
       <Paper className="paper">
         {predefinedFoodDishes.map((foodDish) => {
           const { title, selected } = foodDish;
@@ -21,7 +27,7 @@ const PredefinedFoodDishes = ({ getData, setHits }) => {
               id={title}
               value={title}
               selected={selected}
-              onClick={() => getFoods()}
+              onClick={() => getFoods(title)}
             >
               {title}
             </ToggleButton>
